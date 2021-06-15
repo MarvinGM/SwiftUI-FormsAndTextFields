a40133799653688b8544f679df178e65cce30126
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-15 09:49:27 +0800

    Initial Commit

---
+++ SwiftUI FormsAndTextFields/SwiftUI_FormsAndTextFieldsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormsAndTextFieldsApp.swift
+//  SwiftUI FormsAndTextFields
+//
+//  Created by Amben on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormsAndTextFieldsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
